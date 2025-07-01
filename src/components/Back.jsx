f362cb1c126dfc68d368766506070da108e36dff
Eunwoo Jang <157343502+glsejang@users.noreply.github.com>
2025-07-01 22:48:45 +0900

    많은것들

--- src/components/Back.jsx
+++ src/components/Back.jsx
@@ -3,11 +3,15 @@
 export function Back(){
     return(
         <>
-            <div className="back modal">
-                <div className="profileContainer back">
+            <div className="modal">
+                <div className=" back">
                     <div className="qr">
                         <img src="./images/qr_github.png" alt="" />
-                        back
+                        <span>GitHub</span> 
+                        
+                    </div>
+                    <div className="backInfo">
+                        
                     </div>
 
                 </div>
--- src/components/Project1.jsx
+++ src/components/Project1.jsx
@@ -4,11 +4,24 @@ export function Project1(){
         <>
             <div className="inProject modal p1">
                 <div className="topBox">
-                    <p>프로젝트:<h1>Second Faction</h1></p> <span>(Web blog)</span>
-                    <h2><a href="https://second-faction.onrender.com/" target="_blank" rel="noopener noreferrer" ></a></h2>
+                    <p>프로젝트:</p><h1> Second Faction</h1> 
                 </div>
                 <div className="botBox">
-                    주저리주저리 그렇다.
+                    <div className="botLeft">
+                        <img src="/images/secondfaction.png" alt="" />
+                        <p><a href="https://second-faction.onrender.com/" target="_blank" rel="noopener noreferrer" >링크</a></p>
+                    </div>
+                    <div className="botRight">
+                        <span className="stackBox">Stacks: 자바스크립트 | 파이썬, 플라스크 </span>
+                        <p>제 첫 웹 프로젝트입니다. 첫 프로젝트인만큼 바닐라 자바스크립트와 html, css로 한땀 한땀 만들었습니다.
+                            <br />거기에 웹크롤링을 위한 파이썬 셀레니움, 뷰티풀숲, 간단한 백엔드를 위한 플라스크를 사용했습니다.
+                            <br />
+                            <br />SF소설 서적 및 제 자작 소설 게시 블로그입니다. 
+                            추후 SF영화, 게임 정보 및 관련 큐레이션 추가 예정입니다
+                        </p>
+
+                    </div>
+                    <p></p>
                 </div>
             </div>
         </>
@@ -21,13 +34,25 @@ export function Project2(){
         <>
             <div className="inProject modal p2">
                 <div className="topBox">
-                    <p>프로젝트:<h1>Beef Patty Only</h1></p> <span>(Web blog)</span>
-                    <h2><a href="https://glsejang.github.io/beefpattyonly/" target="_blank" rel="noopener noreferrer" ></a></h2>
+                    <p>프로젝트:</p><h1> Beef Patty Only</h1> 
                 </div>
                 <div className="botBox">
-                    <p>햄버거 리뷰 블로그, 후 실제 배포할 목적으로 제작
-                        <br />햄버거의 색상을 사용
-                    </p>
+                    <div className="botLeft">
+                        <img src="/images/beefpattyonly.png" alt="" />
+                        <p><a href="https://glsejang.github.io/beefpattyonly/" target="_blank" rel="noopener noreferrer" >링크</a></p>
+                    </div>
+                    <div className="botRight">
+                        <span className="stackBox">Stacks: 리액트 | 자바스크립트</span>
+                        <p>처음으로 리액트로 만든 것입니다. 이 웹 역시 나중에 햄버거 리뷰 블로그로 운영하고 싶어서 만들었습니다.
+                            <br />
+                            <br />
+                            헤더와 푸터는 버거 번을, 안의 포스트의 테이블은 패티와 양상추 토마토 등을 표현하려 했습니다.  
+                            <br />
+                            옆의 사이드 내브바는 감자튀김을 표현했습니다.
+                        </p>
+
+                    </div>
+                    <p></p>
                 </div>
             </div>
         </>
